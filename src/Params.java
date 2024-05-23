5b903adb4dfd16c7291020b2001b9881e1fe396f
Neo4Su <suyucheng_andy@sina.com>
2024-05-24 02:37:03 +1000

    test

--- src/Params.java
+++ src/Params.java
@@ -1,13 +1,13 @@
 // Parameters for the simulation
 public class Params {
-    public static final int GRID_WIDTH = 100;
-    public static final int GRID_HEIGHT = 100;
+    public static final int GRID_WIDTH = 40;
+    public static final int GRID_HEIGHT = 40;
 
-    public static final double COP_DENSITY = 0.05;
-    public static final double AGENT_DENSITY = 0.8;
+    public static final double COP_DENSITY = 0.04;
+    public static final double AGENT_DENSITY = 0.7;
 
     public static final int VISION = 7;
-    public static int MAX_JAIL_TERM = 28;
+    public static int MAX_JAIL_TERM = 20;
     public static double LEGITIMACY = 0.8;
     public static boolean MOVEMENT_AGENTS = true;
     public static boolean MOVEMENT_COPS = true;

3071634c35e7d7709333f6ac55e9ae8893dbd015
Neo4Su <suyucheng_andy@sina.com>
2024-05-24 00:09:59 +1000

    test

--- src/Cop.java
+++ src/Cop.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 
 /**
- * Cops(With Extension)
+ * Cops (With Extension)
  * Extension: Instead of moving randomly each turn,
  * cops will move to a cell with higher density of active agents
 */
